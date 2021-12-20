6a1d95cc1f18b24bf6a3b36d4234a862fd1c0f0f
Mimir <mimir@xmppwocky.net>
2021-12-20 12:47:14 -0800

    ?

--- flake.nix
+++ flake.nix
@@ -1,13 +1,17 @@
 {
-  outputs = { nixpkgs }: (
-  {
-    nixopsConfigurations.default = {
-      inherit nixpkgs;
+  outputs = { nixpkgs, ... }: (
+    {
+      nixopsConfigurations.default = {
+        inherit nixpkgs;
 
-      network.description = "fuzzing";
-      fuzz1 = import ./simple-fuzz.nix;
+        network.description = "fuzzing";
+        network.storage.legacy = {
+          databasefile = "~/.nixops/deployments.nixops";
+        };
 
-    };
-  }
+        fuzz1 = import ./simple-fuzz.nix;
+
+      };
+    }
   );
 }
