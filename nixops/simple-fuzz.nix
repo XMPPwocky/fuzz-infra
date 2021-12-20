872905cd29c20a04436b6afd8600aae6da8c4592
Mimir <mimir@xmppwocky.net>
2021-12-20 14:07:12 -0800

    x2

---
+++ nixops/simple-fuzz.nix
@@ -0,0 +1,26 @@
+name:
+fuzzerPkg:
+
+{ config, pkgs, ... }:
+{
+  imports = [ ./vultr.nix ];
+
+  deployment.targetHost = "45.32.226.241";
+  deployment.targetUser = "mimir";
+
+  networking.hostName = "nixos-fuzz-${name}"; # Define your hostname.
+  networking.firewall.allowedTCPPorts = [ 22 80 ];
+
+  environment.systemPackages = with pkgs; [
+    aflplusplus
+    valgrind
+    gdb
+    strace
+    python39
+    fuzzerPkg
+  ];
+
+  systemd.tmpfiles.rules = [
+    "d /fuzz 0700 mimir - - -"
+  ];
+}
